316d2b66be0a81b8a1cc1bdb09970c354ace831c
Siva227 <sankaremany@gmail.com>
2018-02-26 18:21:35 +0530

    Resolve

--- PigLatinMain.java
+++ PigLatinMain.java
@@ -7,6 +7,12 @@ public class PigLatinMain {
 		System.out.println("Enter word for conversion: ");
 		String input = reader.next();
 		reader.close();
-		new PigLatinConverter(input);		
+<<<<<<< HEAD
+		new PigLatinConverter(input);
+		System.out.println("Hello")		
+=======
+		new PigLatinConverter(input);	
+		System.out.println("oneDay! ;)")
+>>>>>>> 915078bb6508485a4136f838d2b54cd4a58e15b4
 	}
 }
