25b6aa1d05633e53327826806ef0918552f62edc
Siva227 <sankaremany@gmail.com>
2018-02-26 16:57:38 +0530

    example

---
+++ PigLatinMain.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PigLatinMain {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter word for conversion: ");
+		String input = reader.next();
+		reader.close();
+		new PigLatinConverter(input);		
+	}
+}
